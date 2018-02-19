ea289d2b7cbb92b988f097affaccc345b9b0b3a8
vaneshita <144995@unsaac.edu.pe>
2018-02-19 06:07:45 -0500

    modificando index.html

--- js/app.js
+++ js/app.js
@@ -13,39 +13,8 @@ $(document).ready(function() {
   {selector: '.animar2',
     offset: 0,
     callback: function() {
-      $('.animar2').animate({right: 30});
-    }},
-  {selector: '.typed2',
-    offset: 0,
-    callback: function() {
-      $('.typed2').animate({right: 30});
+      $('.animar2').animate({right: 40});
     }},
   ];
   Materialize.scrollFire(options);
-  $('#typed').typed({
-    strings: ['VANESSA MENDOZA INOÑAN'],
-    typeSpeed: 30,
-    backDelay: 500,
-    loop: false,
-    loopCount: false,
-    callback: function() {
-      foo();
-    }
-  });
-  function foo() {
-    console.log('Callback');
-  }
-  $('#typed2').typed({
-    strings: ['Desarrolladora Front-End'],
-    typeSpeed: 30,
-    backDelay: 500,
-    loop: false,
-    loopCount: false,
-    callback: function() {
-      foo();
-    }
-  });
-  function foo() {
-    console.log('Callback');
-  }
 });
